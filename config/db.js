5d0f96ea2de3832595f7876e12f2c23d0074e7b4
Siddharth Pandya <sid.pandya66@gmail.com>
2019-02-02 16:26:49 -0500

    displayed db in view

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI" : "mongodb://localhost/test"
+    //"URI" : "mongodb://localhost/test"
+    "URI" : "mongodb://user:password123@ds054999.mlab.com:54999/comp308-w2019-lesson4b"
 }
