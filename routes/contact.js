e5a819afae92925c3755471c29dfb2d64a99bc97
Siddharth Pandya <sid.pandya66@gmail.com>
2019-02-09 14:52:11 -0500

    added Create functionality to database

--- routes/contact.js
+++ routes/contact.js
@@ -3,11 +3,11 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 // create a reference to the database schema
-let contact = require('../models/contact');
+let contactModel = require('../models/contact');
 
 /* GET Contact List page - READ Operation */
 router.get('/', (req,res,next) =>{
-    contact.find((err,contactList) => {
+    contactModel.find((err,contactList) => {
         if(err){
             return console.error(err);
         }
@@ -23,5 +23,37 @@ router.get('/', (req,res,next) =>{
         }
     });
 });
+
+/* GET Route for the Add page
+   this will display the Add Page */
+router.get('/add', (req,res,next) => {
+    res.render('contacts/add', {
+        title: 'Add New Contact'
+    });
+});
+
+/* POST Route for processing the Add page */
+router.post('/add',(req,res,next) =>{
+    //console.log(req.body);
+
+    let newContact = contactModel({
+        "firstName":req.body.firstName,
+        "lastName":req.body.lastName,
+        "age":req.body.age
+    });
+
+    contactModel.create(newContact,(err, contactModel) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //refresh the contact list
+            res.redirect('/contactList');
+        }
+    });
+
+});
+
 
 module.exports = router;
